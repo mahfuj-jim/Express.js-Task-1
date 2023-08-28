dcd17956963e156070a75ccaf28616384d342090
mahfuj-jim <mahfuj.ahmed@bjitacademy.com>
2023-08-28 14:32:27 +0600

    Order & Other Features
    Succesfully Convert Orders & Others Features into Express.js from Node.js

--- app.js
+++ app.js
@@ -134,6 +134,116 @@ app.post("/restaurant/review", async (req, res) => {
   }
 });
 
+app.get("/user/all", async (req, res) => {
+  try {
+    const result = await User.getAllUserData(true);
+    if (result.success) {
+      success(res, "Successfully Received User Data.", result.data);
+    } else {
+      failure(res, result.code, "Failed to get user data", result.error);
+    }
+  } catch (err) {
+    failure(res, 500, "Failed to get user data", "Internal Server Issue");
+  }
+});
+
+app.get("/user/all/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const result = await User.getUserById(userId);
+
+    if (result.success) {
+      success(res, "Successfully Received.", result.data);
+    } else {
+      failure(res, result.code, "Failed to get data", result.error);
+    }
+  } catch (err) {
+    failure(res, 500, "Failed to get data", "Internal Server Issue");
+  }
+});
+
+app.get("/order/all", async (req, res) => {
+  try {
+    const result = await Order.getAllOrderData(true);
+
+    if (result.success) {
+      success(res, "Successfully Received.", result.data);
+    } else {
+      failure(res, result.code, "Failed to get data", result.error);
+    }
+  } catch (err) {
+    failure(res, 500, "Failed to get data", result.error);
+  }
+});
+
+app.get("/order", async (req, res) => {
+  try {
+    const { id, restaurant_id, user_id } = req.query;
+
+    if (id !== undefined) {
+      const result = await Order.getOrderById(id);
+      if (result.success) {
+        success(res, "Successfully Received.", result.data);
+      } else {
+        failure(res, result.code, "Failed to get data", result.error);
+      }
+    } else if (restaurant_id !== undefined) {
+      const result = await Order.getRestaurantOrder(restaurant_id);
+      if (result.success) {
+        success(res, "Successfully Received.", result.data);
+      } else {
+        failure(res, result.code, "Failed to get data", result.error);
+      }
+    } else if (user_id !== undefined) {
+      const result = await Order.getUserOrder(user_id);
+      if (result.success) {
+        success(res, "Successfully Received.", result.data);
+      } else {
+        failure(res, result.code, "Failed to get data", result.error);
+      }
+    } else {
+      failure(res, 400, "Failed to connect", "Request not found");
+    }
+  } catch (err) {
+    failure(res, 500, "Failed to get data", "Internal Server Error");
+  }
+});
+
+app.put('/order/status', async (req, res) => {
+  try {
+      const {orderId, order_status} = req.query;
+
+      if (order_status === 'done') {
+          const result = await Order.completeOrder(orderId);
+
+          if (result.success) {
+              success(res, 'Successfully Done.', result.data);
+          } else {
+              failure(res, result.code, 'Failed to update', result.error);
+          }
+      } else {
+          failure(res, 400, 'Failed to connect', 'Invalid request');
+      }
+  } catch (err) {
+    console.log(err);
+      failure(res, 500, 'Failed to update', 'Internal Server Error');
+  }
+});
+
+app.post('/order/create', async (req, res) => {
+  try {
+      const result = await Order.createOrder(JSON.parse(req.body));
+
+      if (result.success) {
+          success(res, 'Successfully Created.', result.data);
+      } else {
+          failure(res, result.code, 'Failed to create new order', result.error);
+      }
+  } catch (err) {
+      failure(res, 500, 'Failed to create new order', 'Internal Server Issue');
+  }
+});
+
 app.use((req, res) => {
   failure(res, 400, "Not Found", "Request not found");
 });
