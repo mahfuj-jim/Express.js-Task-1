446c45c1fe18c4372e3e956a37365a3aaa25d594
mahfuj-jim <mahfuj.ahmed@bjitacademy.com>
2023-09-04 14:04:06 +0600

    Login

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -109,7 +109,64 @@ class AuthController {
   }
 
   async login(req, res) {
-    failure(res, 500, "Failed to get data", "Internal Server Issue");
+    try {
+      const { email, password } = JSON.parse(req.body);
+
+      AuthModel.findOne({ email })
+        .populate("user")
+        .populate("restaurant")
+        .exec()
+        .then(async (auth) => {
+          const isPasswordValid = await bcrypt.compare(password, auth.password);
+
+          if (!isPasswordValid) {
+            return failure(res, 401, "Login failed", "Invalid Credential");
+          }
+
+          return success(
+            res,
+            "Authentication successful",
+            auth.role === 1 ? auth.user : auth.restaurant
+          );
+
+          //   const token = jwt.sign(
+          //     {
+          //       restaurant: {
+          //         _id: restaurant._id,
+          //         name: restaurant.name,
+          //         location: restaurant.location,
+          //         cuisine: restaurant.cuisine,
+          //         rating: restaurant.rating,
+          //         contactNumber: restaurant.contactNumber,
+          //         owner: restaurant.owner,
+          //         email: restaurant.email,
+          //       },
+          //       role: "restaurant",
+          //     },
+          //     process.env.ACCESS_TOKEN_SECRET
+          //   );
+
+          //   return success(res, "Authentication successful", {
+          //     token,
+          //     restaurant: {
+          //       id: restaurant._id,
+          //       name: restaurant.name,
+          //       location: restaurant.location,
+          //       cuisine: restaurant.cuisine,
+          //       rating: restaurant.rating,
+          //       contactNumber: restaurant.contactNumber,
+          //       owner: restaurant.owner,
+          //       email: restaurant.email,
+          //     },
+          //   });
+        })
+        .catch((error) => {
+          return failure(res, 401, "Login failed", "Invalid Credential");
+        });
+    } catch (err) {
+      console.log(err);
+      failure(res, 500, "Login failed", "Internal Server Issue");
+    }
   }
 }
 
