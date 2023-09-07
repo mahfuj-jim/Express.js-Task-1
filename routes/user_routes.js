59041ad820de2ec4ddea2365239d54dad03bb471
mahfuj-jim <mahfuj.ahmed@bjitacademy.com>
2023-09-07 07:50:01 +0600

    Order Validation

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,10 +1,26 @@
 const UserController = require("../controllers/user_controller.js");
-const { validateToken, validateAdminToken, validateOrderCreateToken } = require("../middleware/token_validation");
+const {
+  validateToken,
+  validateAdminToken,
+  validateOrderCreateToken,
+} = require("../middleware/token_validation");
+const { validateCart } = require("../middleware/user_validator.js");
 const express = require("express");
 const router = express.Router();
 
-router.get("/all", validateToken, validateAdminToken, UserController.getAllUserData);
+router.get(
+  "/all",
+  validateToken,
+  validateAdminToken,
+  UserController.getAllUserData
+);
 router.get("/all/:user_id", validateToken, UserController.getUserById);
-router.post("/cart/:user_id", validateToken, validateOrderCreateToken, UserController.addToCart);
+router.post(
+  "/cart/:user_id",
+  validateCart,
+  validateToken,
+  validateOrderCreateToken,
+  UserController.addToCart
+);
 
 module.exports = router;
